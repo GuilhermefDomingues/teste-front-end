71eef15fa5e40102a85cc2514ab32a4273eb31cb
GuilhermefDomingues <guilhermefrancadomingues@outlook.com>
2026-05-18 12:10:55 -0300

    fix: use Econverse products API

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,9 @@
 import type { Product } from "../types/product";
 
 export async function getProducts(): Promise<Product[]> {
-  const response = await fetch("/api/produtos.json");
+  const response = await fetch(
+    "https://app.econverse.com.br/teste-front-end/junior/tecnologia/lista-produtos/produtos.json"
+  );
 
   if (!response.ok) {
     throw new Error("Erro ao buscar os produtos.");
